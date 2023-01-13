8eed7cba448354a8e5839b9433cb6a60002524e5
AvantiDharme <122555335+AvantiDharme@users.noreply.github.com>
2023-01-13 12:15:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,38 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
-basic.showIcon(IconNames.Yes)
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . # . # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # # . .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . . . .
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
